f756446d856124a82aa96d5a456e8c02f948c046
Andres Ruiz <Andres.ruiz3561@gmail.com>
2022-02-03 09:51:00 -0800

    just some cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.cascadeCommand;
 import frc.robot.commands.shooterCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.cascade;
 import frc.robot.subsystems.drive;
 import frc.robot.subsystems.shooter;
@@ -18,15 +11,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  
+
   private final shooter shooterSub = new shooter();
   private final drive driveSub = new drive();
   private final cascade cascadeSub = new cascade();
@@ -37,12 +23,11 @@ public class RobotContainer {
 
   Joystick jasmine = new Joystick(0);
 
-  RunCommand move = new RunCommand(() -> driveSub.move(.7 * jasmine.getRawAxis(1),
-  .7 * jasmine.getRawAxis(5)),driveSub);
+  RunCommand move = new RunCommand(() -> driveSub.move(Constants.driveSpeed*jasmine.getRawAxis(1),
+  Constants.driveSpeed*jasmine.getRawAxis(5)),driveSub);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
+
     JoystickButton shooter = new JoystickButton(jasmine, 1);
     shooter.whenPressed(shooterMove);
 
@@ -53,21 +38,11 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+ 
   private void configureButtonBindings() {}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+ 
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
     return null;
   }
 }
--- src/main/java/frc/robot/subsystems/drive.java
+++ src/main/java/frc/robot/subsystems/drive.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
@@ -11,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class drive extends SubsystemBase {
+
   WPI_TalonFX rightFront = new WPI_TalonFX(Constants.rightFront);
   WPI_TalonFX rightRear = new WPI_TalonFX(Constants.rightRear);
   WPI_TalonFX leftFront = new WPI_TalonFX(Constants.leftFront);
@@ -18,19 +15,19 @@ public class drive extends SubsystemBase {
 
   MotorControllerGroup rightDrive = new MotorControllerGroup(rightFront, rightRear);
   MotorControllerGroup leftDrive = new MotorControllerGroup(leftFront, leftRear);
+
   DifferentialDrive drive = new DifferentialDrive(leftDrive, rightDrive);
   
 
   /** Creates a new drive. */
 
   public void move(double leftSpeed,double rightSpeed){
-  drive.tankDrive( -1 *leftSpeed, rightSpeed);
+  drive.tankDrive( -1 *leftSpeed,rightSpeed);
 
   }
   public drive() {}
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
