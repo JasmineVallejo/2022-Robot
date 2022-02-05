5994747b1e45d8843f2abdeead150d41f21b0768
Jasmine Vallejo <jasmine.vallejo123@gmail.com>
2022-02-04 23:25:57 -0800

    :)

--- src/main/java/frc/robot/commands/indexerCommand.java
+++ src/main/java/frc/robot/commands/indexerCommand.java
@@ -1,6 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot.commands;
 
@@ -15,26 +13,25 @@ public class indexerCommand extends CommandBase {
     speed = indexerSpeed;
     indexerSub = indexer;
     addRequirements(indexerSub);
-    // Use addRequirements() here to declare subsystem dependencies.
+   
   }
 
-  // Called when the command is initially scheduled.
+  
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
+  
   @Override
   public void execute() {
     indexerSub.move(speed);
   }
 
-  // Called once the command ends or is interrupted.
+  
   @Override
   public void end(boolean interrupted) {
     indexerSub.move(0);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
