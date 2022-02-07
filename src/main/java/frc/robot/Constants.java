b13a42e92dcf537d31983f23c8691f4745a3cb1d
JasmineVallejo <jasmine.vallejo123@gmail.com>
2022-02-06 21:14:42 -0800

    added button

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,16 +18,16 @@ indexMotor = 17,
 
 //----------------------------------- Buttons -----------------------------------------------
 rotaryForwardButton = 1, rotaryBackwardButton = 2, cascadeDownButton = 3, cascadeUpButton = 4, 
-IndexerInButton = 5, IndexerOutButton = 7, shooterButton = 6;
+IndexerInButton = 5, IndexerOutButton = 7, shooterButton = 6, leftDriveAxis = 1, rightDriveAxis = 5, aimRobotButton = 8;
 
 
 public static double
 
 
 // ----------------------------------- Vision Tracking -------------------------------------
-hubHeight = 8.667, limelightHeight = 2.25, bottomAngle = 15, desiredDistance = 6,
+hubHeight = 8.667, limelightHeight = 2.25, bottomAngle = 15, targetDistance = 10,
 // ----------------------------------- PIDS for shooter -------------------------------------
-aimKP = .1, aimKI = .1, aimKD = .1, distanceKP = .1, distanceKI =.01, distanceKD = .01, 
+aimKP = .1, aimKI = 0, aimKD = 0, distanceKP = .1, distanceKI = 0, distanceKD = 0, 
 
 //----------------------------------- Encoder -----------------------------------------------
 kEncoderDistancePerPulse = ((Math.PI * 6)/24576),
